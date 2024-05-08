305e9dd5dd0d6620a2411c904e9ae8cff6b843dc
mattdaboi <146141224+mattdaboi@users.noreply.github.com>
2024-05-08 17:16:07 -0400

    Update ConnectFour.py

--- ConnectFour.py
+++ ConnectFour.py
@@ -1,35 +1,57 @@
-#This class models the Connect-four main loop for our game.
-#All in game moves are found here.
+"""
+Module name: ConnectFour.py
+Author: Matthew Moga
+Date: January 5, 2024
+Description: This class models the Connect Four main loop for our game. All in game moves are found here.
+"""
 
 
-import random
+
+
+#Import statement to get Board from Board.py
 from Board import Board
 
 
+
+
 class ConnectFour:
+    """
+    This function initializes all the instance variables (no instance variables).
+    """
     def __init__(self):
         pass
     
-    #This method defines the mainline logic for our game loop. 
-    #It uses a Connect-Four Board object and various helper methods to run the game.
+
+
+
+    """
+    This function defines the mainline logic for our game loop. It uses various helper methods to run the game.
+    """
     @staticmethod
     def main():
+        #Instantiate board
         connectFourBoard = Board()
         print("\nWelcome to Connect-Four!\n")
         playGame = False
         gameLoop = False
+        #Prompt user for name
         name = input("What is your name? ")
-            
+        
+        #Prompt user for yes or no response
         answer = input("\nYou ready for a challenge, " + name + "?\n(...YES or NO...) ")
         if answer.lower() == "yes" or "yes" in answer:
+            #Start game
             print("\nPrepare yourself for the greatest match of Connect-Four...\n")
             playGame = True
             gameLoop = True
+        
+        #Exit game loop
         elif answer.lower() == ("no") or "no" in answer:
             print("\nUnfortunate. Practice more and then come back...\n")
         else:
             print("\nNot sure what you mean there, " + name + ". Get serious then come back!!!\n")
-            
+        
+        #Mainline logic
         while (gameLoop):
             playGame = True
             while (playGame):
@@ -41,14 +63,17 @@ class ConnectFour:
                     ConnectFour.makeComputerMove(connectFourBoard)
                         
                 usersTurn = not usersTurn
+                #Win scenario
                 if connectFourBoard.checkWinner('X') == True:
                     print("YOU Won!  Congratulations.\n")
                     playGame = False
 
+                #Lose scenario
                 elif connectFourBoard.checkWinner('O') == True:
                     print("You LOSE!  Heehee!\n")
                     playGame = False
 
+                #Draw scenario
                 else:
                     if connectFourBoard.getEmptyCells() == 0:
                         print("Tie!  Try harder!\n")
@@ -57,11 +82,18 @@ class ConnectFour:
             print("*** G A M E   O V E R ***")
             connectFourBoard.printBoard()
             connectFourBoard.clearBoard()
+            #Ask user to play again
             gameLoop = ConnectFour.askPlayAgain()
-        
-                
-    #This method prompts the user for a move.
-    #When this method exits, an 'X' will be placed on the board in a valid (empty) cell.
+                  
+
+
+
+    """
+    This function prompts the user for a move. When this function exits, an 'X' will be placed on the board in a valid (empty) cell.
+
+    Args:
+        connectFourBoard (char array): Board that is used to place 'X' token on
+    """
     @staticmethod
     def makeUserMove(connectFourBoard):
         validMove = False
@@ -74,25 +106,32 @@ class ConnectFour:
             if not validMove:
                 print("Sorry, that location is not available to place an 'X'.\n")
         
-        
-    #This method generates a computer move with the use of CPU education and advanced machine learning.
-    #When this method exits, an 'O' will be placed on the board in a valid (empty) cell.
+    
+
+
+    """
+    This method generates a computer move with the use of CPU education and advanced machine learning. When this method exits, an 'O' will be placed on the board in a valid (empty) cell.
+
+    Args:
+        connectFourBoard (char array): Board that is used to place 'O' token on
+    """
     @staticmethod
     def makeComputerMove(connectFourBoard):
         validMove = False
         col = -1                
             
-        #Keep generating random moves until we find an empty cell to place an 'O'
+        #Generate computer move, the column where we find an empty cell to place an 'O'
         while not validMove:
             col = connectFourBoard.computerAttack()
-            #placeToken() will return false if the cell is not empty (i.e., we need to try a new spot)
             validMove = connectFourBoard.placeToken(col, 'O')
         
 
-    #This method prompts the user if they would like to play again with some invalidation.
+
+    """
+    This function prompts the user if they would like to play again with some input validation.
+    """
     @staticmethod
     def askPlayAgain():
-        #After reading integer values for row/col we need to consume the left-over <return> on the Scanner.
         playAgain = input("Play again? [y/n]: ")
         while (not playAgain == ("y") and not playAgain == ("yes") and not playAgain == ("n") and not playAgain == ("no")):
             playAgain = input("Play again? [y/n]: \n")
@@ -101,4 +140,8 @@ class ConnectFour:
             return True
         return False
 
+
+
+
+#Call the main method
 ConnectFour.main()

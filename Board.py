00a4d36858409ab790aa7a00b1bf0ad40141065e
Matt M <146141224+Matt1092@users.noreply.github.com>
2025-01-04 00:44:00 -0500

    Update Board.py

--- Board.py
+++ Board.py
@@ -80,5 +80,100 @@ class Board:
                     return True
         return False
 
+    def checkAcross(self, row):
+        for col in range(self.getCols() - 2):
+            if self.board[row][col] == 'X' and self.board[row][col + 1] == 'X' and self.board[row][col + 2] == ' ' and ((row + 1 < self.getRows()) and (self.board[row + 1][col + 2] == 'X' or self.board[row + 1][col + 2] == 'O')):
+                return col + 2
+            elif self.board[row][col] == ' ' and ((row + 1 < self.getRows()) and (self.board[row + 1][col] == 'X' or self.board[row + 1][col] == 'O')) and self.board[row][col + 1] == 'X' and self.board[row][col + 2] == 'X':
+                return col
+            elif self.board[row][col] == 'X' and self.board[row][col + 1] == 'X' and self.board[row][col + 2] == ' ':
+                return col + 2
+            elif self.board[row][col] == ' ' and self.board[row][col + 1] == 'X' and self.board[row][col + 2] == 'X':
+                return col
+        return -1
+
+    def checkVertical(self, col):
+        for row in range(self.getRows() - 2):
+            if self.board[row + 2][col] == 'X' and self.board[row + 1][col] == 'X' and self.board[row][col] == ' ':
+                return True
+        return False
+
+    def blockVertical(self, col):
+        for row in range(self.getRows() - 3):
+            if self.board[row + 3][col] == 'X' and self.board[row + 2][col] == 'X' and self.board[row + 1][col] == 'X' and self.board[row][col] == ' ':
+                return True
+        return False
+
+    def blockAcross(self, row):
+        for col in range(self.getCols() - 3):
+            if self.board[row][col] == 'X' and self.board[row][col + 1] == 'X' and self.board[row][col + 2] == 'X' and self.board[row][col + 3] == ' ' and ((row + 1 < self.getRows()) and (self.board[row + 1][col + 3] == 'X' or self.board[row + 1][col + 3] == 'O')):
+                return col + 3
+            elif self.board[row][col] == ' ' and ((row + 1 < self.getRows()) and (self.board[row + 1][col] == 'X' or self.board[row + 1][col] == 'O')) and self.board[row][col + 1] == 'X' and self.board[row][col + 2] == 'X' and self.board[row][col + 3] == 'X':
+                return col
+            elif self.board[row][col] == 'X' and self.board[row][col + 1] == 'X' and self.board[row][col + 2] == 'X' and self.board[row][col + 3] == ' ':
+                return col
+            elif self.board[row][col] == ' ' and self.board[row][col + 1] == 'X' and self.board[row][col + 2] == 'X' and self.board[row][col + 3] == 'X':
+                return col
+        return -1
+
+    def checkWinAcross(self, row):
+        for col in range(self.getCols() - 3):
+            if self.board[row][col] == 'O' and self.board[row][col + 1] == 'O' and self.board[row][col + 2] == 'O' and self.board[row][col + 3] == ' ' and ((row + 1 < self.getRows()) and (self.board[row + 1][col + 3] == 'X' or self.board[row + 1][col + 3] == 'O')):
+                return col + 3
+            elif self.board[row][col] == ' ' and ((row + 1 < self.getRows()) and (self.board[row + 1][col] == 'X' or self.board[row + 1][col] == 'O')) and self.board[row][col + 1] == 'O' and self.board[row][col + 2] == 'O' and self.board[row][col + 3] == 'O':
+                return col
+            elif self.board[row][col] == 'O' and self.board[row][col + 1] == 'O' and self.board[row][col + 2] == 'O' and self.board[row][col + 3] == ' ':
+                return col
+            elif self.board[row][col] == ' ' and self.board[row][col + 1] == 'O' and self.board[row][col + 2] == 'O' and self.board[row][col + 3] == 'O':
+                return col
+        return -1
+
+    def checkWinVertical(self, col):
+        for row in range(self.getRows() - 3):
+            if self.board[row + 3][col] == 'O' and self.board[row + 2][col] == 'O' and self.board[row + 1][col] == 'O' and self.board[row][col] == ' ':
+                return True
+        return False
+
+    def computerAttack(self):
+        """
+        This function implements the above helper functions to determine the best possible computer move.
+
+        Returns:
+            int: Specified column to place the 'O'.
+        """
+        # Check for potential horizontal winning move
+        for row in range(self.getRows()):
+            if self.checkWinAcross(row) != -1:
+                return self.checkWinAcross(row)
+        
+        # Check for potential vertical winning move
+        for col in range(self.getCols()):
+            if self.checkWinVertical(col):
+                return col
+        
+        # Check for win stopping blocks (horizontal and vertical)
+        for row in range(self.getRows()):
+            if self.blockAcross(row) != -1:
+                return self.blockAcross(row)
+        for col in range(self.getCols()):
+            if self.blockVertical(col):
+                return col
+
+        # Check for strategic horizontal blocks
+        for row in range(self.getRows()):
+            if self.checkAcross(row) != -1:
+                return self.checkAcross(row)
+        
+        # Check for strategic vertical blocks
+        for col in range(self.getCols()):
+            if self.checkVertical(col):
+                return col
+
+        # Make random move otherwise (ensuring cell is empty)
+        for row in range(self.getRows() - 1, -1, -1):
+            for col in range(self.getCols()):
+                if self.board[row][col] == ' ':
+                    return col
+
     def __str__(self):
-        return f"Statistics: \n" + "There are: " + str(self.getEmptyCells()) + " empty cells."
+        return f"Statistics: \nThere are: {self.getEmptyCells()} empty cells."
